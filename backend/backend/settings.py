c6d31faa7be5f2120b820bc5b97c7ad6532b585c
ErfanGerami <e.geramizadeh13821359@gmail.com>
2025-03-23 12:23:47 +0330

    reomving env files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -207,7 +207,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "e.geramizadeh13821359@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get(
-    "EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.environ.get("EMAIL", "e.geramizadeh13821359@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
