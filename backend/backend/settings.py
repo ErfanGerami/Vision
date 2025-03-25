5642ad94d7b1c1c06b6a6f1226297eeabffa3457
ErfanGerami <e.geramizadeh13821359@gmail.com>
2025-03-25 05:22:34 +0330

    removing cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,10 +31,10 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_ALL_ORIGINS = True  # Allows any domain
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_METHODS = ["*"]
-CORS_ALLOWED_ORIGINS = ["*"]
 
 
 # Application definition
